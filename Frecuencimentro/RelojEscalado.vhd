158c4c38d54dedd8d1cf2ff3768d0d564a93e52e
AngelLM <alm37454@gmail.com>
2014-12-02 18:45:19 +0100

    Angel Reloj

---
+++ Frecuencimentro/RelojEscalado.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    18:37:14 12/02/2014 
+-- Design Name: 
+-- Module Name:    RelojEscalado - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RelojEscalado is
+    Port ( clk : in  STD_LOGIC;
+           ms_signal : out  STD_LOGIC);
+end RelojEscalado;
+
+architecture Behavioral of RelojEscalado is
+
+begin
+
+
+end Behavioral;
+
